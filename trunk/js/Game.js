4edd5eb13e88e6301902ad5b62bbd46023462c8c
sethgalbraith <sethgalbraith@4ead51ca-c501-11de-a2e8-8d36a3108c74>
2010-09-29 07:38:51 +0000

    new approach to account management menu, avoids submit button glitches in chrome.

--- trunk/js/Game.js
+++ trunk/js/Game.js
@@ -75,11 +75,11 @@ var Game = {
   },
   
   login: function(username, password) {
-    Game.loggedInMode();
+    Game.setUserForm("loggedInUserForm");
   },
   
   logout: function() {
-    Game.defaultMode();
+    Game.setUserForm("defaultUserForm");
   },
 
   changePassword: function(oldPassword, newPassword) {
@@ -88,93 +88,15 @@ var Game = {
   deleteUser: function() {
   },
 
-  clearLoginForm: function () {
-    while (Game.loginForm.firstChild) {
-      Game.loginForm.removeChild(Game.loginForm.firstChild);
+  setUserForm: function(formId) {
+    var node = Game.userForm.firstChild;
+    while (node) {
+      if (node.nodeType == document.ELEMENT_NODE) {
+        node.style.display = "none";
+      }
+      node = node.nextSibling;
     }
-  },
-
-  defaultMode: function () {
-    Game.clearLoginForm();
-    var login = Game.createElement("input", {type: "submit", value: "log in"}, Game.loginForm);
-    login.onclick = function () {Game.loginMode();};
-    var register = Game.createElement("input", {type: "submit", value: "register"}, Game.loginForm);
-    register.onclick = function () {Game.registerMode();};
-  },
-
-  loginMode: function () {
-    Game.clearLoginForm();
-    var username = Game.createElement("input", {type: "text", placeholder: "username"}, Game.loginForm);
-    var password = Game.createElement("input", {type: "password", placeholder: "password"}, Game.loginForm);
-    var login = Game.createElement("input", {type: "submit", value: "log in"}, Game.loginForm);
-    login.onclick = function () {Game.login(username.value, password.value);};
-    var cancel = Game.createElement("input", {type: "submit", value: "cancel"}, Game.loginForm);
-    cancel.onclick = function () {Game.defaultMode();};
-  },
-
-  registerMode: function () {
-    Game.clearLoginForm();
-    var username = Game.createElement("input", {type: "text", placeholder: "new username"}, Game.loginForm);
-    var password = Game.createElement("input", {type: "password", placeholder: "new password"}, Game.loginForm);
-    var retype = Game.createElement("input", {type: "password", placeholder: "type new password again"}, Game.loginForm);
-    var register = Game.createElement("input", {type: "submit", value: "register"}, Game.loginForm);
-    register.onclick = function () {
-      if (password.value != retype.value) {
-        alert("The passwords you typed do not match.");
-      }
-      else {
-        Game.createUser(username.value, password.value);
-      }
-    };
-    var cancel = Game.createElement("input", {type: "submit", value: "cancel"}, Game.loginForm);
-    cancel.onclick = function () {Game.defaultMode();};
-  },
-
-  loggedInMode: function () {
-    Game.clearLoginForm();
-    var logout = Game.createElement("input", {type: "submit", value: "log out"}, Game.loginForm);
-    logout.onclick = function () {Game.logout();};
-    var options = Game.createElement("input", {type: "submit", value: "account options"}, Game.loginForm);
-    options.onclick = function () {Game.optionsMode();};
-  },
-
-  optionsMode: function () {
-    Game.clearLoginForm();
-    var password = Game.createElement("input", {type: "submit", value: "change password"}, Game.loginForm);
-    password.onclick = function () {Game.passwordMode();};
-    var erase = Game.createElement("input", {type: "submit", value: "delete my account"}, Game.loginForm);
-    erase.onclick = function () {Game.deleteMode();};
-    var cancel = Game.createElement("input", {type: "submit", value: "cancel"}, Game.loginForm);
-    cancel.onclick = function () {Game.loggedInMode();};
-  },
-
-  passwordMode: function () {
-    Game.clearLoginForm();
-    var old = Game.createElement("input", {type: "password", placeholder: "old password"}, Game.loginForm);
-    var password = Game.createElement("input", {type: "password", placeholder: "new password"}, Game.loginForm);
-    var retype = Game.createElement("input", {type: "password", placeholder: "type new password again"}, Game.loginForm);
-    var change = Game.createElement("input", {type: "submit", value: "change password"}, Game.loginForm);
-    change.onclick = function () {
-      if (password.value != retype.value) {
-        alert("The passwords you typed do not match.");
-      }
-      else {
-        Game.createUser(old.value, password.value);
-      }
-    };
-    var cancel = Game.createElement("input", {type: "submit", value: "cancel"}, Game.loginForm);
-    cancel.onclick = function () {Game.optionsMode();};
-  },
-
-  deleteMode: function () {
-    Game.clearLoginForm();
-    Game.loginForm.appendChild(document.createTextNode(
-      "Are you sure you want to delete your account? "
-    + "This operation cannot be undone. All your saved games will be lost."));
-    var erase = Game.createElement("input", {type: "submit", value: "delete my account"}, Game.loginForm);
-    erase.onclick = function () {Game.deleteUser();};
-    var cancel = Game.createElement("input", {type: "submit", value: "cancel"}, Game.loginForm);
-    cancel.onclick = function () {Game.optionsMode();};
+    document.getElementById(formId).style.display = "inline";
   }
 
 };
@@ -187,9 +109,55 @@ addEventListener('load', function() {
   // USER INTERFACE
 
   Game.container = document.getElementById("game");
-  Game.loginForm = document.getElementById("login");
+  Game.userForm = document.getElementById("userForm");
   Game.menu = Game.createElement("div", {className: "menu"}, Game.container);
-  Game.defaultMode();
+
+  // User Account Form
+
+  Game.setUserForm("defaultUserForm");
+  document.getElementById("showLogin").onclick = function () {
+    Game.setUserForm("loginUserForm");};
+  document.getElementById("showRegister").onclick = function () {
+    Game.setUserForm("registerUserForm");};
+  document.getElementById("login").onclick = function () {
+    var username = document.getElementById("loginUserName").value;
+    var password = document.getElementById("loginPassword").value;
+    Game.login(username, password);
+  };
+  document.getElementById("cancelLogin").onclick = function () {
+    Game.setUserForm("defaultUserForm");};
+  document.getElementById("register").onclick = function () {
+    var username = document.getElementById("registerUserName").value;
+    var password = document.getElementById("registerPassword").value;
+    var retype = document.getElementById("registerRetype").value;
+    if (password != retype) alert("The passwords you typed do not match.");
+    else Game.createUser(username, password);
+  };
+  document.getElementById("cancelRegister").onclick = function () {
+    Game.setUserForm("defaultUserForm");};
+  document.getElementById("logout").onclick = function () {
+    Game.logout();};
+  document.getElementById("showAccountOptions").onclick = function () {
+    Game.setUserForm("optionsUserForm");};
+  document.getElementById("showChangePassword").onclick = function () {
+    Game.setUserForm("passwordUserForm");};
+  document.getElementById("showDeleteUser").onclick = function () {
+    Game.setUserForm("deleteUserForm");};
+  document.getElementById("cancelAccountOptions").onclick = function () {
+    Game.setUserForm("loggedInUserForm");};
+  document.getElementById("changePassword").onclick = function () {
+    var oldPassword = document.getElementById("passwordOld").value;
+    var newPassword = document.getElementById("passwordNew").value;
+    var retype = document.getElementById("passwordRetype").value;
+    if (newPassword != retype) alert("The new passwords you typed do not match.");
+    else Game.createUser(oldPassword, newPassword);
+  };
+  document.getElementById("cancelChangePassword").onclick = function () {
+    Game.setUserForm("optionsUserForm");};
+  document.getElementById("deleteUser").onclick = function () {
+    Game.deleteUser();};
+  document.getElementById("cancelDeleteUser").onclick = function () {
+    Game.setUserForm("optionsUserForm");};
 
 }, true);
 
