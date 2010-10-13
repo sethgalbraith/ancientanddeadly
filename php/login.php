17d264a71425d95ae40c00647620a9b61c3847c8
sethgalbraith <sethgalbraith@4ead51ca-c501-11de-a2e8-8d36a3108c74>
2010-10-13 19:08:51 +0000

    add remaining PHP scripts

---
+++ php/login.php
@@ -0,0 +1,32 @@
+<?php
+
+session_start();
+
+include('db_config.php');
+include('include/query.php');
+include('include/users.php');
+
+// Failure for any reason results in an empty (whitespace?) document.
+// We return same failure result regardless of the reason for failure
+// so that we don't help password crackers figure out if they got the 
+// wrong password, the wrong username or the wrong argument names.
+
+// Interpret the Request
+
+$username = $AD_SQL->real_escape_string($_REQUEST['username']);
+$password = $AD_SQL->real_escape_string($_REQUEST['password']);
+
+// Query the Database and Generate Output
+
+if ($rows = AD_call_silent('read_user_by_name', $username)) {
+  $hash = AD_hash_password($password, $rows[0]['password_salt']);
+  if (strcmp($hash, $rows[0]['password_hash']) == 0) {
+    // Save session variables that only the server can modify.
+    $_SESSION['username'] = $rows[0]['name'];
+    // indicate a successful login
+    echo $username . " logged in.";
+  }
+}
+
+?>
+
