b7db68511531bbd4dbf1bc8e77615730d9aba6cb
sethgalbraith <sethgalbraith@4ead51ca-c501-11de-a2e8-8d36a3108c74>
2010-10-13 16:13:15 +0000

    accidentally converted salt_ to saAD_

--- trunk/php/include/users.php
+++ trunk/php/include/users.php
@@ -5,14 +5,14 @@ function AD_random_salt() {
 
   // 26 capital letters + 26 lowercase letters + 10 numerals = 62 characters
   // base 2 log of 62 possible characters = 5.95 bits of entropy per character
-  $saAD_chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  $salt_chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
   // 5.95 bits/character x 22 characters = 131 bits of entropy
-  $saAD_length = 22;
+  $salt_length = 22;
 
   $salt = "";
-  for ($i = 0; $i < $saAD_length; $i++) {
-    $salt .= $saAD_chars[rand(0, strlen($saAD_chars) - 1)];
+  for ($i = 0; $i < $salt_length; $i++) {
+    $salt .= $salt_chars[rand(0, strlen($salt_chars) - 1)];
   }
 
   return $salt;
@@ -27,8 +27,6 @@ function AD_write_user_row($row) {
   // URL-encode strings to be decoded by javascript's decodeURIComponent.
   echo "  <user id=\"{$row['id']}"
     . "\" name=\"" . rawurlencode($row['name'])
-    . "\" color=\"" . rawurlencode($row['color'])
-    . "\" permissions=\"" . rawurlencode($row['permissions'])
     . "\"/>\n";
 }
 
