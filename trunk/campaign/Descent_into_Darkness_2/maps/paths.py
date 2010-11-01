efbf524080c1235dbb56431d2f1febc0219013b4
sethgalbraith <sethgalbraith@4ead51ca-c501-11de-a2e8-8d36a3108c74>
2010-11-01 21:02:01 +0000

    new paths, pointed in right direction

--- trunk/campaign/Descent_into_Darkness_2/maps/paths.py
+++ trunk/campaign/Descent_into_Darkness_2/maps/paths.py
@@ -1,10 +1,10 @@
 STROKE = {
   "ffffff": ("ruins", "windmill", "north", "south"),
-  "eeeeee": ("ruins", "tent", "west", "east"),
+  "eeeeee": ("tent", "ruins", "east", "west"),
   "dddddd": ("ruins", "shop", "south", "south"),
-  "cccccc": ("start", "shop", "south", "east"),
-  "bbbbbb": ("start", "beacon", "north", "south"),
-  "aaaaaa": ("fort", "cellar", "south", "north"),
+  "cccccc": ("shop", "start", "east", "south"),
+  "bbbbbb": ("beacon", "start", "south", "north"),
+  "aaaaaa": ("cellar", "fort", "north", "south"),
   "999999": ("camp", "well", "south", "east"),
   "888888": ("cemetery", "hills", "east", "west"),
   "777777": ("cabin", "mine", "west", "east"),
@@ -16,16 +16,16 @@ STROKE = {
   "111111": ("start", "ruins", "west", "east")}
 STYLE = "       style=\"fill:none;stroke:#"
 PATH = "       d=\"M "
+FILENAME = "Parthyn.svg"
 
 def xmlPath(points, orig, dest, direction):
-  print '  <path from="%s" to="%s" direction="%s">' % (orig, dest, direction)
+  print '    <path from="%s" to="%s" direction="%s">' % (orig, dest, direction)
   for p in points:
-    print '    <p x="%s" y="%s" />' % tuple(p)
-  print '  </path>'
+    print '      <p x="%s" y="%s" />' % tuple(p)
+  print '    </path>'
 
-for line in open("Parthyn.svg"):
+for line in open(FILENAME):
   if line[:len(STYLE)] == STYLE:
-    i = line.find("#");
     color = line[len(STYLE):len(STYLE) + 6]
     origin, destination, direction1, direction2 = STROKE[color]
   if line[:len(PATH)] == PATH:
@@ -33,4 +33,6 @@ for line in open("Parthyn.svg"):
     points = [point.split(",") for point in coordinates.split(" ")]
     xmlPath(points, origin, destination, direction1)
     xmlPath(reversed(points), destination, origin, direction2)
+
+
 
